441d4e117e4454c63d040bef3fa6c2c4f8718509
Carmofrasao <anderfrasao52@gmail.com>
2022-08-28 16:56:22 -0300

    ..

--- cliente.c
+++ cliente.c
@@ -17,7 +17,7 @@ int main(){
     // soquete = ConexaoRawSocket("lo");
     soquete = ConexaoRawSocket("enp2s0f1");     // note anderson
     // soquete = ConexaoRawSocket("enp2s0");    // desktop anderson
-    // soquete = ConexaoRawSocket("eno1");      // desktop andesor c3
+    // soquete = ConexaoRawSocket("eno1");      // desktop andersor c3
     // soquete = ConexaoRawSocket("enp3s0");    // note gobbo
 
     struct timeval tv;
